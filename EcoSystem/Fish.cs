a7a681c4eb4345be06c7c3df29d936de063c25dc
Dima <dmitiyoleynik@gmail.com>
2019-12-04 13:36:04 +0200

    Changed fish behavior

--- EcoSystem/Fish.cs
+++ EcoSystem/Fish.cs
@@ -51,6 +51,11 @@ namespace EcoSystem
             {
                 return;
             }
+            if (CurrentTime >= timeToDie)
+            {
+                Die();
+                return;
+            }
 
             if (CurrentTime >= timeToReproduce)
             {
